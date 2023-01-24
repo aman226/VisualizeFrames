bc755266e0dd3539db304b150605c58b3a189f31
Aman Kumar Singh <amanmain3@gmail.com>
2023-01-24 08:57:59 +0530

    Ability to Switch Reference Frames

--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 from frameviz import FrameVisualization
 import numpy as np
 
-ft = FrameVisualization([-4,4],[-4,4],[0,8])
-initial_position = np.array([0,0,0])
+
+ft = FrameVisualization([-4,4],[-4,4],[-4,4],0.5)
+initial_position = np.array([2.0,2.0,2.0])
 basis_vector = np.array([ [1.0 , 0.0, 0.0], 
                           [0.0 , 1.0, 0.0], 
-                          [0.0 , 0.0, 1.0]])
-ft.addFrame("Ground", initial_position, basis_vector,0.2)
+                          [0.0 , 0.0, 1.0] ])
 ft.addFrame("Precession", initial_position, basis_vector)
+
 while True:
     for angle in np.linspace(0,2*np.pi,300):
+        ft.changeFrame("World")
+        # Update in World Frame
         ft.updateFrame("Precession",initial_position, basis_vector)
         ft.applyEulerRotation("Precession",angle,np.pi/6,angle,"ZYZ")
+
+        # Change to Precession Frame
+        ft.changeFrame("Precession")
+
+        # Display Frames wrt Precession Frame
         ft.displayAllFrames(0.01)
+        
